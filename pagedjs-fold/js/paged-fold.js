d0077818004ab8880f61072e4a0ddc1651f6334d
julientaq <julien@lesvoisinsdustudio.ch>
2026-01-12 22:40:10 +0100

    fix pagenumbers

--- pagedjs-fold/js/paged-fold.js
+++ pagedjs-fold/js/paged-fold.js
@@ -13,8 +13,11 @@ class MyHandler extends Paged.Handler {
 
   afterRendered(pages, chunker) {
     document.querySelector("template").remove();
-    //remove all the previous print rules
-    removePrintRules();
+
+    // Remove all the previous print rules if needed, but i’m not so sure about that
+    // removePrintRules();
+
+    // From there, i would replace all the numbering of the page by hand if needed, since we’re moving things around
 
     let wrapper = document.createElement("div");
     wrapper.classList.add("page-wrapper");
@@ -27,6 +30,9 @@ class MyHandler extends Paged.Handler {
     for (let i = 1; i < printedpages; i++) {
       let newpage = document.createElement("div");
       newpage.classList.add("newpage");
+
+      // some cheats to avoid counter issues
+      newpage.style.counterReset = `page ${4 * (i - 1)}`;
       // this new page must get settings.pagePerSheet number of pages
       for (let j = 0; j < settings.pagePerSheet; j++) {
         if (!document.querySelector(".pagedjs_pages .pagedjs_page")) break;
@@ -40,6 +46,8 @@ class MyHandler extends Paged.Handler {
     document.body.insertAdjacentElement("beforeend", wrapper);
     document.body.insertAdjacentHTML("afterbegin", `<style>${style}</style>`);
     document.querySelector(".pagedjs_pages").remove();
+    document.body.innerHTML =
+      document.body.querySelector("page-wrapper").innerHTML;
   }
 }
 
@@ -52,41 +60,50 @@ size: ${settings.outputSize.width} ${settings.outputSize.height};
 margin: 0 !important;
 }
 
-
+@media screen {
+.page-wrapper {
+    display: flex;
+    flex-direction: column;
+}
 .newpage {
-break-before: page;
-display: grid;
-width: ${settings.outputSize.width} ;
-height: ${settings.outputSize.height};
-overflow: hidden;
-padding: 0;
-grid-template-columns: repeat(2,var(--pagedjs-width));
-grid-template-rows: repeat(2,var(--pagedjs-height));
-.pagedjs_page {
-&:nth-of-type(1) {
-   background: purple;
-   grid-column: 1;
-   grid-row: 1;
-   transform: rotate(180deg);
+    margin: 2em auto;
 }
-&:nth-of-type(2) {
-   background: green;
-   grid-column: 1;
-   grid-row: 2;
 }
-&:nth-of-type(3) {
-   background: orange;
-   grid-column: 2;
-   grid-row: 2;
-}
-&:nth-of-type(4) {
-   background: blue;
-   grid-column: 2;
-   grid-row: 1;
-   transform: rotate(180deg);
-}
+.newpage {
+    break-before: page;
+    display: grid;
+    width: ${settings.outputSize.width} ;
+    height: ${settings.outputSize.height};
+    overflow: hidden;
+    padding: 0;
+grid-template-columns: repeat(${settings.pagePerSheet / 2}, var(--pagedjs-width));
+grid-template-rows: repeat(${settings.pagePerSheet / 2}, var(--pagedjs-height));
+    background: white;
+    .pagedjs_page {
+        &:nth-of-type(1) {
+           background: purple;
+           grid-column: 1;
+           grid-row: 1;
+           transform: rotate(180deg);
+        }
+        &:nth-of-type(2) {
+           background: green;
+           grid-column: 1;
+           grid-row: 2;
+        }
+        &:nth-of-type(3) {
+           background: orange;
+           grid-column: 2;
+           grid-row: 2;
+        }
+        &:nth-of-type(4) {
+           background: blue;
+           grid-column: 2;
+           grid-row: 1;
+           transform: rotate(180deg);
+        }
 
-}
+    }
 }
 
 `;
